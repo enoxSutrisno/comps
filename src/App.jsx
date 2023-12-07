d3e2b1162e0cc9f80faec093a932df6e0fb2219d
enoxSutrisno <sutrisnoenox27@gmail.com>
2023-12-07 14:04:26 +0700

    feat: add button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import Button from "./Button"
+import { CgArrowTopLeftR, CgArrowsExpandLeftAlt } from "react-icons/cg";
+
 
 function App() {
+  const handleClick = () => {
+    console.log('click')
+  }
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <Button primary rounded onClick={handleClick}>
+          <CgArrowTopLeftR />
+          Clcik</Button>
+      </div>
+
+      <div>
+        <Button secondary outline onMouseLeave={handleClick}>
+          <CgArrowsExpandLeftAlt />
+          buy now</Button>
+      </div>
+      <div>
+        <Button success outline>see deal</Button>
+      </div>
+      <div>
+        <Button warning outline>hide ads</Button>
+      </div>
+      <div>
+        <Button danger outline>somethng</Button>
+      </div>
+    </div>
   )
 }
 
